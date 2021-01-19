58045790eb404cd82f5d17fe025d28358717deed
John Clayton <johnclayton72@gmail.com>
2021-01-19 18:02:09 +0100

    clean up

--- development/Tools.ps1
+++ development/Tools.ps1
@@ -1,4 +1,3 @@
-choco install -y sysinternals
 #choco install -y ngrok.portable
 #choco install -y python3
 #choco install -y fiddler
@@ -6,14 +5,17 @@ choco install -y sysinternals
 #choco install -y rapidee
 #choco install -y snoop
 #choco install -y SourceTree
-#choco install -y notepadplusplus
-choco install -y nodejs
 
-# tools to ease life working with Windows
-choco install -y cmdermini
 #choco install -y paint.net
 #choco install -y SwissFileKnife
 #choco install -y windirstat
 #choco install -y rufus
+
+choco install -y sysinternals
+choco install -y notepadplusplus
+choco install -y nodejs
+
+# tools to ease life working with Windows
+choco install -y cmdermini
 choco install -y javaruntime
 choco install -y steam

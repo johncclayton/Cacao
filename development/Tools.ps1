0ff987ef2ae9f8ae71ad5524b7e22a464d33e2f7
John Clayton <johnclayton72@gmail.com>
2021-02-14 18:54:47 +0100

    added windirstat

--- development/Tools.ps1
+++ development/Tools.ps1
@@ -18,5 +18,6 @@ choco install -y nodejs
 # tools to ease life working with Windows
 choco install -y javaruntime
 choco install -y steam
+choco install -y windirstat
 
 choco install -y 1Password -force

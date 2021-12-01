76c5ac93d70c2b52d67e464464f82708cf5472be
John Clayton <johnclayton72@gmail.com>
2021-12-01 15:45:51 +0100

    Mods

--- development/Tools.ps1
+++ development/Tools.ps1
@@ -4,6 +4,7 @@ choco install -y notepadplusplus
 choco install -y javaruntime
 choco install -y steam logitech-options
 choco install -y windirstat
+choco install -y hugo-extended
 
 choco install -y 1Password -force
 choco install -y 1password-cli

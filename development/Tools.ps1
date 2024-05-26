4403e5443679e4dfbf654398eea66403fbfad102
John Clayton <invoice@effective-flow.ch>
2024-05-26 13:47:29 +0200

    Update Tools.ps1

--- development/Tools.ps1
+++ development/Tools.ps1
@@ -5,6 +5,7 @@ choco install -y javaruntime
 choco install -y steam
 choco install -y windirstat
 choco install -y hugo-extended
+choco install -y audacity
 
 choco install -y 1Password -force
 choco install -y 1password-cli
